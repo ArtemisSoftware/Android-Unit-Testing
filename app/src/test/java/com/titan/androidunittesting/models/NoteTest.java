996320e87307f4a5d416219935197add959340f7
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-12-05 19:09:40 +0000

    Writing Your First Unit Tests

--- app/src/test/java/com/titan/androidunittesting/models/NoteTest.java
+++ app/src/test/java/com/titan/androidunittesting/models/NoteTest.java
@@ -2,40 +2,119 @@ package com.titan.androidunittesting.models;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class NoteTest {
 
 
-    /*
-        Compare two equal Notes
+    public static final String TIMESTAMP_1 = "05-2019";
+    public static final String TIMESTAMP_2 = "04-2019";
+
+
+
+    /**
+     * Compare two equal Notes
+     * @throws Exception
      */
-
-
     @Test
-    void isNotesEqual_identicalProperties() throws Exception {
+    void isNotesEqual_identicalProperties_returnTrue() throws Exception {
 
-        //Arrange
+        // Arrange
+        Note note1 = new Note("Note #1", "This is note #1", TIMESTAMP_1);
+        note1.setId(1);
+        Note note2 = new Note("Note #1", "This is note #1", TIMESTAMP_1);
+        note2.setId(1);
 
-        //Act
+        // Act
 
-        //Assert
+
+        // Assert
+        assertEquals(note1, note2);
+        System.out.println("The notes are equal!");
 
     }
 
-    /*
-        Compare notes with 2 different ids
+
+    /**
+     * Compare notes with 2 different ids
+     * @throws Exception
      */
+    @Test
+    void isNotesEqual_differentIds_returnFalse() throws Exception {
+        // Arrange
+        Note note1 = new Note("Note #1", "This is note #1", TIMESTAMP_1);
+        note1.setId(1);
+        Note note2 = new Note("Note #1", "This is note #1", TIMESTAMP_1);
+        note2.setId(2);
 
-    /*
-        Compare two notes with different timestamps
+        // Act
+
+
+        // Assert
+        assertNotEquals(note1, note2);
+        System.out.println("The notes are not equal!");
+    }
+
+
+    /**
+     * Compare two notes with different timestamps
+     * @throws Exception
      */
+    @Test
+    void isNotesEqual_differentTimestamps_returnTrue() throws Exception {
+        // Arrange
+        Note note1 = new Note("Note #1", "This is note #1", TIMESTAMP_1);
+        note1.setId(1);
+        Note note2 = new Note("Note #1", "This is note #1", TIMESTAMP_2);
+        note2.setId(1);
+
+        // Act
 
 
-    /*
-        Compare two notes with different titles
+        // Assert
+        assertEquals(note1, note2);
+        System.out.println("The notes are equal!");
+    }
+
+
+    /**
+     * Compare two notes with different titles
+     * @throws Exception
      */
+    @Test
+    void isNotesEqual_differentTitle_returnFalse() throws Exception {
+        // Arrange
+        Note note1 = new Note("Note #1", "This is note #1", TIMESTAMP_1);
+        note1.setId(1);
+        Note note2 = new Note("Note #2", "This is note #1", TIMESTAMP_2);
+        note2.setId(1);
+
+        // Act
 
 
-    /*
-        Compare two notes with different content
+        // Assert
+        assertNotEquals(note1, note2);
+        System.out.println("The notes are not equal! They have different titles.");
+    }
+
+
+    /**
+     * Compare two notes with different content
+     * @throws Exception
      */
+    @Test
+    void isNotesEqual_differentContent_returnFalse() throws Exception {
+        // Arrange
+        Note note1 = new Note("Note #1", "This is note #1", TIMESTAMP_1);
+        note1.setId(1);
+        Note note2 = new Note("Note #1", "This is note #2", TIMESTAMP_2);
+        note2.setId(1);
+
+        // Act
+
+
+        // Assert
+        assertNotEquals(note1, note2);
+        System.out.println("The notes are not equal! They have different content.");
+    }
 }
