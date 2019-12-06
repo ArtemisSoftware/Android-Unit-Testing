f031c8b34bbbbc27a1f3c6e9badbfc6b4c14b780
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-12-06 19:46:57 +0000

    Room DAO Testing (part 1) (Instrumentation)

---
+++ app/src/androidTest/java/com/titan/androidunittesting/NoteDaoTest.java
@@ -0,0 +1,24 @@
+package com.titan.androidunittesting;
+
+public class NoteDaoTest extends NoteDatabaseTest{
+
+    /*
+    Insert, read, delete
+     */
+
+    /*
+    Insert, Read, update, read, delete
+     */
+
+
+    /*
+    Insert note with null title, throw exception
+     */
+
+    /*
+    Insert, Update with null title, throw exceptio
+     */
+
+
+
+}
