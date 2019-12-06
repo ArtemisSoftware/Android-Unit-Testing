ef48cf12db599af19c6fcd9e2a7d4736c533e684
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-12-06 19:11:43 +0000

    Room Persistence Setup (SQLite Db)

--- app/src/main/java/com/titan/androidunittesting/di/AppModule.java
+++ app/src/main/java/com/titan/androidunittesting/di/AppModule.java
@@ -1,7 +1,29 @@
 package com.titan.androidunittesting.di;
 
+import android.app.Application;
+
+import androidx.room.Room;
+
+import com.titan.androidunittesting.persistence.NoteDao;
+import com.titan.androidunittesting.persistence.NoteDataBase;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public class AppModule {
+
+    @Singleton
+    @Provides
+    static NoteDataBase provideNoteDatabase(Application application){
+        return Room.databaseBuilder(application, NoteDataBase.class, NoteDataBase.DATABASE_NAME).build();
+    }
+
+    @Singleton
+    @Provides
+    static NoteDao provideNoteDao(NoteDataBase noteDataBase){
+        return noteDataBase.getNoteDao();
+    }
 }
